1f04c7dac4e2427f9f2b400007604237e9f52090
AshishKumarJena <ashishkujena2000@gmail.com>
2022-06-10 14:39:29 +0530

    Implemented testing using JUnit

--- src/test/java/com/ashish/junit/helper/StringHelperTest.java
+++ src/test/java/com/ashish/junit/helper/StringHelperTest.java
@@ -1,14 +1,22 @@
 package com.ashish.junit.helper;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class StringHelperTest {
 
 	// AACD => CD ACD => CD CDEF => CDEF CDAA => CDAA
 
-	StringHelper helper = new StringHelper();
+	StringHelper helper;
+	
+	@Before
+	public void before() {
+		helper = new StringHelper();
+	}
 
 	@Test
 	public void testTruncateAInFirst2Positions_Ainfirst2Positions() {
@@ -24,5 +32,16 @@ public class StringHelperTest {
 	}
 
 	// expected:<ABC[]> but was:<ABC[D]>
+
+	// ABCD => false, ABAB => true, AB => true, A => false
+	@Test
+	public void testAreFirstAndLastTwoCharactersTheSame_BasicNegativeScenario() {
+		assertFalse(helper.areFirstAndLastTwoCharactersTheSame("ABCD"));
+	}
+	
+	@Test
+	public void testAreFirstAndLastTwoCharactersTheSame_BasicPositiveScenario() {
+		assertTrue(helper.areFirstAndLastTwoCharactersTheSame("ABAB"));
+	}
 
 }
